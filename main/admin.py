287e737c7f5a158783bf2e87da1a904353c6f519
aitolkun <aitolkuns52@gmail.com>
2022-12-07 13:01:41 +0600

    admin

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Category,Product
 
-# Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['tiltle', 'category','status']
+    list_filter  = ['category', 'status']
+    search_fields= ['title', 'decription']
+
+admin.site.register(Category)
+admin.site.register(Product, ProductAdmin)
+
