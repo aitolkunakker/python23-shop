731308d87ce819b39e051bb61a96d5f9fc77f073
aitolkun <aitolkuns52@gmail.com>
2022-12-08 12:31:57 +0600

    filter + search + page

--- config/settings.py
+++ config/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     'account',
     'main',
+    'django_filters',
     
 
 ]
@@ -141,7 +142,11 @@ AUTH_USER_MODEL = 'account.User'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
     'rest_framework_simplejwt.authentication.JWTAuthentication',
-)
+    
+),
+'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 2,
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
 }
 
 from datetime import timedelta
--- main/views.py
+++ main/views.py
@@ -1,6 +1,12 @@
 from rest_framework.viewsets import ModelViewSet
+from  rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.permissions import  IsAdminUser
 from .serializers import CategorySerializer, ProductSerializer
 from .models import Category, Product
+from .filters import ProductFilter
+from django.db.models import Q
+
 
 
 class CategoryViewSet(ModelViewSet):
@@ -10,4 +16,29 @@ class CategoryViewSet(ModelViewSet):
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    filterset_class = ProductFilter
+    # filterset_fields = ['category', 'status']
+    
+
+    def get_permissions(self):
+        if self.action in ['retrieve','list','search']:
+            # если это запрос на листинг или детализацию
+            return []
+        return [IsAdminUser()] #разрешвем только админам
+    @action(['GET'], detail=False)
+    def search(self, request):
+        q = request.query_params.get('q')
+        queryset = self.get_queryset()
+        if q:
+            queryset = queryset.filter(Q(tiltle__icontains=q) | Q(description__icontains=q) )
+            # queryset = Product.objects.filter(tiltle__icontains=q)
+        pagination = self.paginate_queryset(queryset)
+        if pagination:
+            serializer= self.get_serializer(pagination, many=True)
+            return self.get_paginated_response(serializer.data)
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data, status=200)
+
+
+
 
